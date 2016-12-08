ba8c5917a0c165119ed29a3072713ecc4296c034
user <user@0587337214.wireless.umich.net>
2016-12-07 21:31:41 -0500

    setup for test data mostly

--- feature_vector.py
+++ feature_vector.py
@@ -9,10 +9,8 @@ from process_training import *
 from sets import Set
 
 
-def make_feature_vector(tweets, uni_features, feature_vector, sentiment, labels, invert_index, idf_dict, unigram_scores):
-    print("Making feature vector...");
-    le = preprocessing.LabelEncoder()
-    le.fit(list(corpus))
+def make_feature_vector(tweets, uni_features, feature_vector, sentiment, labels, invert_index, idf_dict, unigram_scores, state):
+    print "Making", state, "feature vector..."
 
     for tweet in tweets:
         features = dict.fromkeys(uni_features, 0)
@@ -27,17 +25,12 @@ def make_feature_vector(tweets, uni_features, feature_vector, sentiment, labels,
                     features[token] = unigram_scores[token] * tfidf                    
 
 
-        # #abbrev, sl, and conj are my feature choices
-        # abbrev = is_abbreviation(word) #want word included with period
-        # sl = sentence_len_constaint(i, last_index) #senctence lenght, includes , and ; though
-        # num = int(unicode(L, 'utf-8').isnumeric()) #if is number
-        # features.append(sl)    
-
         feature_vector.append(features.values())
         del features
-        labels.append(sentiment[tweet])
+        if state == "training":
+            labels.append(sentiment[tweet])
     #print feature_vector
-    print("Feature vector complete");
+    print state, "feature vector complete."
 
 
 sentiment, tweets = get_training_data2()
@@ -68,7 +61,39 @@ features = list(test_unigrams & NRC_unigrams)
 
 training_vector = []
 training_labels = []
-make_feature_vector(tweets, features, training_vector, sentiment, training_labels, training_index, idf_dict, unigram_scores)
+
+make_feature_vector(tweets, features, training_vector, sentiment, training_labels, training_index, idf_dict, unigram_scores, "training")
+
+
+test_vector = []
+tmp = []
+make_feature_vector(messages, features, test_vector, sentiment, tmp, training_index, idf_dict, unigram_scores, "testing")
+
+def mydist(x, y, **kwargs):
+    return np.sum((x-y)**kwargs["power"])
+
+
+X = np.array(training_vector)
+Y = np.array(labels)
+nbrs = KNeighborsClassifier(n_neighbors=1, algorithm='ball_tree',metric=mydist, metric_params={"power": 2})
+nbrs.fit(X, Y)
+Z = np.array(test_vector)
+dists, inds = nbrs.kneighbors(Z)
+
+f = open('labeled_messages.txt','w')
+j = 0
+for message in messages:
+    line += messages[message] + ' ' + labels[inds[j]]
+    line += '\n'
+    j+=1
+    f.write(line)
+
+
+
+#make feature vector for 
+
+
+
 
 
     
